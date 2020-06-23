7596ce27f16dd485b896279a5ad91b75a070f260
shanon <13fritsh@gmail.com>
2020-06-23 01:02:31 -0500

    added input changers, signup submit button, and useeffects for disabled button

--- potluckplanner/src/App.js
+++ potluckplanner/src/App.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Login from './components/login';
 import Signup from './components/signup';
+import loginSchema from './components/formSchemalogin';
+import signupSchema from './components/formSchemaSignup';
+import * as yup from 'yup';
+import axios from 'axios';
 
 const initialLoginValues = {
   username: '',
@@ -68,8 +72,81 @@ function App() {
   };
 
   const signupInputChange = (event) =>{
+    const {name, value} = event.target;
+
+    yup
+      .reach(signupSchema, name)
+      .validate(value)
+      .then(response =>{
+        setSignupErrors({
+          ...signupErrors,
+          [name]: ''
+        });
+      })
+      .catch(error =>{
+        setSignupErrors({
+          ...signupErrors,
+          [name]: error.errors[0]
+        });
+      });
+
+    setSignup({
+      ...signup,
+      [name]: value
+    });
+  };
+
+  const loginInputChange = (event) =>{
+    const {name, value} = event.target;
     
-  }
+    yup
+      .reach(loginSchema, name)
+      .validate(value)
+      .then(response =>{
+        setLoginErrors({
+          ...loginErrors,
+          [name]: ''
+        });
+      })
+      .catch(error =>{
+        setLoginErrors({
+          ...loginErrors,
+          [name]: error.errors[0]
+        });
+      });
+
+    setLogin({
+      ...login,
+      [name]: value
+    });
+  };
+
+  const signupSubmit = (event) =>{
+    evt.preventDefault();
+
+    const newMember = {
+      firstName: signup.firstName.trim(),
+      lastName: signup.lastName.trim(),
+      username: signup.username.trim(),
+      email: signup.email.trim(),
+      password: signup.password.trim(),
+      rptPassword: signup.rptPassword.trim(),
+    };
+
+    postMember(newMember);
+  };
+
+  useEffect(() =>{
+    signupSchema.isValid(signup).then(valid =>{
+      setDisabled(!valid)
+    });
+  }, [signup]);
+
+  useEffect(() =>{
+    loginSchema.isValid(login).then(valid =>{
+      setDisabled(!valid)
+    });
+  }, [login]);
 
   return (
     <div className="App">
