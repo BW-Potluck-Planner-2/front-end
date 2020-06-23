4b28b48b4554609ab4745dede735f01b247bfcd6
shanon <13fritsh@gmail.com>
2020-06-23 16:44:41 -0500

    Updates

---
+++ precache-manifest.d3f40b5395970e66da93095347739e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c6dbbf22b5e967183f18db00ba8ea9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e4f8f616d7c3a9a4fa0a",
+    "url": "/my-app/static/css/main.97e4b0dc.chunk.css"
+  },
+  {
+    "revision": "cd518cf5a16b2b0af46c",
+    "url": "/my-app/static/js/2.04965269.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.04965269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f8f616d7c3a9a4fa0a",
+    "url": "/my-app/static/js/main.06cb876f.chunk.js"
+  },
+  {
+    "revision": "7be1157482f36c119084",
+    "url": "/my-app/static/js/runtime-main.293d142a.js"
+  }
+]);
